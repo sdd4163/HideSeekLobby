aad00e90769958e585c237e1f127dd56f4b51db0
Steven Dill <sdd4163@rit.edu>
2015-12-13 19:50:10 -0500

    Combined map generation and proper Hider/Seeker spawning. Made some general improvements to project's organization, especially the game scene.  Minor fixes done like removing the colliders on the glass panes.

---
+++ HideSeekLobby/Assets/Scripts/MapCreation.cs
@@ -0,0 +1,125 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using UnityEngine.Networking.NetworkSystem;
+
+public class MapCreation : NetworkBehaviour {
+
+    SyncListInt SyncListMapIndex = new SyncListInt();
+    List<Vector3> positions = new List<Vector3>();
+    const int mazepieces = 11;
+    //index codex
+    //0 = hider spawn point
+    //1 = seeker spawn point
+    //2+ = terrain/obsticales
+    [SerializeField]
+    public GameObject HiderSpawn;
+    [SerializeField]
+    public GameObject SeekerSpawn;
+    [SerializeField]
+    public GameObject Terrain1;
+    [SerializeField]
+    public GameObject Terrain2;
+    [SerializeField]
+    public GameObject Terrain3;
+    [SerializeField]
+    public GameObject Terrain4;
+    public bool host;
+    GameObject[] terrains = new GameObject[6];
+    bool first = true;
+    bool instantiated = false;
+    bool generated = false;
+	// Use this for initialization
+	void Start () {
+        if (first)
+        {
+            Vector3 a = new Vector3(0, 0, 0);
+            for (int i = 0; i < mazepieces; i++)
+            {
+                GenerateMap();      
+            }
+          
+            
+            first = false;
+            generated = true;
+        }
+        if (instantiated == false)
+        {
+            InstantiateMap();
+        }
+	}
+
+	// Update is called once per frame
+	void Update () {
+        
+            
+	}
+    
+    void Awake()
+    {
+        positions.Add(new Vector3(0, 0, 0));
+        positions.Add(new Vector3(50, 0, 0));
+        positions.Add(new Vector3(100, 0, 0));
+        positions.Add(new Vector3(150, 0, 0));
+        positions.Add(new Vector3(200, 0, 0));
+        positions.Add(new Vector3(0, 0, 50));
+        positions.Add(new Vector3(0, 0, 100));
+        positions.Add(new Vector3(0, 0, 150));
+        positions.Add(new Vector3(0, 0, 200));
+        positions.Add(new Vector3(0, 0, 250));
+        positions.Add(new Vector3(50, 0, 50));
+        
+    }
+   
+    
+    void InstantiateMap()
+    {
+  
+        terrains[0] = HiderSpawn;
+        terrains[1] = SeekerSpawn;
+        terrains[2] = Terrain1;
+        terrains[3] = Terrain2;
+        terrains[4] = Terrain3;
+        terrains[5] = Terrain4;
+        int counter=0;
+        if (generated)
+        {
+            for (int k = 0; k < mazepieces; k++)
+            {
+                Vector3 origin = positions[k];
+
+                for (int i = 0; i < 5; i++)
+                {
+                    for (int j = 0; j < 5; j++)
+                    {
+                        Debug.Log(SyncListMapIndex.Count);
+                        Instantiate(terrains[SyncListMapIndex[counter]], new Vector3(origin.x + i * 10, origin.y + 0, origin.z + j * 10), new Quaternion(0, 0, 0, 0));
+                        counter++;
+                    }
+                }
+            }
+        }
+        instantiated = true;
+    }
+    [Server]
+    void GenerateMap()
+    {
+       
+       for(int i =0; i<25;i++)
+       {
+           
+           SyncListMapIndex.Add(Random.Range(0, 6));
+               
+       }
+       
+
+    }
+    [Server]
+    void ReleaseMap()
+    {
+        SyncListMapIndex.Clear();
+    }
+
+}
---
+++ HideSeekLobby/Assets/Scripts/PlayerNetStartup.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayerNetStartup : NetworkBehaviour
+{
+
+	public Camera FPSCam;
+	public AudioListener audioListener;
+
+	[SyncVar]
+	public bool isHider = false;
+	
+	void Start ()
+	{
+		/*
+		 * We turned off the character controller, firstperson controller, 
+		 * the main camera and its audiolistener, because only OUR OWN character should respond to 
+		 * keyboard events and control the camer, not all the other clients.
+		 * 
+		 * So here we turn them all back on IF IT'S US. 
+		 * */
+		GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+		bool hiderMade = false;
+		foreach (GameObject p in players){
+			if (p.GetComponent<PlayerNetStartup>().isHider){
+				hiderMade = true;
+			}
+		}
+
+		if (isLocalPlayer) {
+			GameObject.Find ("Scene Camera").SetActive (false);
+			GetComponent<CharacterController> ().enabled = true;
+			if (hiderMade){
+				GetComponent<Seeker>().enabled = true;
+				Destroy(GetComponent<Hider>());
+				gameObject.name = "Seeker";
+			}
+			else{
+				isHider = true;
+				GetComponent<Hider>().enabled = true;
+				Destroy(GetComponent<Seeker>());
+				gameObject.name = "Hider";
+			}
+			GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController> ().enabled = true;
+			FPSCam.enabled = true;
+			audioListener.enabled = true;
+		}
+		else if (isHider){
+			GetComponent<Hider>().enabled = true;
+			Destroy(GetComponent<Seeker>());
+			gameObject.name = "Hider";
+		}
+		else{
+			GetComponent<Seeker>().enabled = true;
+			Destroy(GetComponent<Hider>());
+			gameObject.name = "Seeker";
+		}
+	}
+}
